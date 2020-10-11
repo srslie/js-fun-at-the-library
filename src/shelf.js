1b1bb421aee832263fc86678c4574788a8bd4f3f
Alice Ruppert <aliceruppert@gmail.com>
2020-10-11 02:50:26 -0600

    Add passing shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,33 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3)
+  shelf.unshift(book)
+};
+
+function unshelfBook(title, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+        shelf.splice(i, 1)
+      }
+  }
+};
+
+function listTitles(shelf) {
+  var titles = []
+  for (var i = 0 ; i < shelf.length; i++) {
+    titles.push(shelf[i].title)
+  }
+  return titles.join(', ')
+};
+
+function searchShelf(shelf, title) {
+  var titles = listTitles(shelf)
+  return titles.includes(title)
+};
 
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
