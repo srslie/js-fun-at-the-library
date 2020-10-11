4d0bde4ef3198b72dcaa25e345b4b786ead89f88
Alice Ruppert <aliceruppert@gmail.com>
2020-10-11 02:26:06 -0600

    Add passing book.js

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return 'The ' + title
 }
+
+function buildMainCharacter(characterName, characterAge, characterPronouns) {
+  return {
+    name: characterName,
+    age: characterAge,
+    pronouns: characterPronouns
+  }
+};
+
+function saveReview(review, reviewArray) {
+  if (!reviewArray.includes(review)) {
+  reviewArray.push(review)
+  }
+};
+
+function calculatePageCount(title) {
+  return title.length * 20
+};
+
+function writeBook(bookTitle, bookCharacter, bookGenre) {
+  return {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: bookGenre
+  }
+};
+
+function editBook(book) {
+  book.pageCount = book.pageCount * .75
+};
+
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
-  // saveReview: saveReview,
-  // calculatePageCount: calculatePageCount,
-  // writeBook: writeBook,
-  // editBook: editBook
+  buildMainCharacter: buildMainCharacter,
+  saveReview: saveReview,
+  calculatePageCount: calculatePageCount,
+  writeBook: writeBook,
+  editBook: editBook
 }
