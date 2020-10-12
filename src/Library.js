d070df1144a9eeab036e7350c5b3be2796018057
Alice Ruppert <aliceruppert@gmail.com>
2020-10-12 00:10:47 -0600

    Add passing library.js

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,35 @@
+function createLibrary(libraryName) {
+  return {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    }
+  }
+};
+
+
+function addBook(library, book) {
+  library.shelves[book.genre].push(book)
+};
+
+
+function checkoutBook(library, bookTitle) {
+  for (shelfKey in library.shelves) {
+    for (var i = 0; i < library.shelves[shelfKey].length; i++) {
+      if (bookTitle === library.shelves[shelfKey][i].title) {
+        library.shelves[shelfKey].splice(i, 1)
+        return `You have now checked out ${bookTitle} from the ${library.name}`
+      }
+    }
+   }
+  return `Sorry, there are currently no copies of ${bookTitle} available at the ${library.name}`
+}
 
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
-  // checkoutBook: checkoutBook
+  createLibrary: createLibrary,
+  addBook: addBook,
+  checkoutBook: checkoutBook
 };
--- src/librarian.js
+++ src/librarian.js
@@ -2,10 +2,25 @@ var checkoutBook = require('../src/library.js').checkoutBook;
 var searchShelf = require('../src/shelf.js').searchShelf;
 
 class Librarian {
-  constructor() {
-
+  constructor(librarianName, library) {
+    this.name = librarianName;
+    this.library = library;
   }
-  
-}
 
+  greetPatron(patron, isMorning) {
+    return isMorning ? `Good morning, ${patron}!` : `Hello, ${patron}!`
+  }
+
+  findBook(title) {
+    if (library.shelves.fantasy.includes(title)) {
+      return `Yes, we have ${tile}`
+    } else if (library.shelves.nonFiction.includes(title)) {
+      return `Yes, we have ${tile}`
+    } else if (library.shelves.fiction.includes(title)) {
+      `Yes, we have ${tile}`
+    } else {
+      return `Sorry, we do not have ${title}`
+  }
+}
+} 
 module.exports = Librarian;
